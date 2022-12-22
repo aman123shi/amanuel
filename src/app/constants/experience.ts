cfb2cc590ce99c805a37ea920c824c40a435a48e
Amanuel Shiferaw <123aman64@gmail.com>
2022-12-22 09:36:41 +0300

    Add Experience and Skills to Constants

--- src/app/constants/experience.ts
+++ src/app/constants/experience.ts
@@ -1,28 +1,49 @@
 export interface ExperienceDTO {
-   organization:string,
-   position:string,
-   organizationDescription:string,
-   majorRole:string,
-   roles:string[],
-   technologies:string[],
-   languages:string[]
+  organization: string;
+  position: string;
+  organizationDescription: string;
+  majorRole: string;
+  roles: string[];
+  technologies: string[];
+  languages: string[];
 }
 
-export const experiences:ExperienceDTO[] = [
-  {organization:"Jimma Institute of Technologies",
-  position:"Working as java developer for ICT Development center",
- organizationDescription:"Jimma Institute of Technology (JIT) is a leading institution of technology in Ethiopia and an emerging regional and global hub of innovative research and teaching activity.",
- majorRole:"Junior Software Engineer (Intern)",
- roles:["Software Development","Application Development"],
- technologies:["Agile","Google Maps","Open street Maps","Location API","Git","Linux"],
- languages:["Java","Servlet","MySQL","Bash_Script"]
-},
-{organization:"Backend development for Freelance",
-position:"Working as Backend developer",
-organizationDescription:"Developing A Restful API for E-commerce system using Node.js with Test driven Development Pattern with clean coding and proper documentation.",
-majorRole:"Software Engineer,Programmer",
-roles:["Software Development","Database Designer","Tester"],
-technologies:["TDD","Agile","Swagger","JWT","Git","Linux"],
-languages:["Node.js","Express","MongoDB","Jest"]
-}
-]; 
+export const experiences: ExperienceDTO[] = [
+  {
+    organization: 'COYDOE',
+    position: 'Working as Full-stack Developer mainly NestJs and React',
+    organizationDescription:
+      "Consortium of Youth Dev't organization in Ethiopia is a regional and global (NGO) working with youths achieve their goal through different trainings.",
+    majorRole: 'Senior Software Engineer ',
+    roles: ['Software Development', 'Team Leader'],
+    technologies: ['Docker', 'CI/CD', 'Redux Toolkit', 'Scrum', 'Git', 'Linux'],
+    languages: ['Typescript', 'React', 'NestJs', 'MongoDB'],
+  },
+  {
+    organization: 'Jimma Institute of Technologies',
+    position: 'Working as java developer for ICT Development center',
+    organizationDescription:
+      'Jimma Institute of Technology (JIT) is a leading institution of technology in Ethiopia and an emerging regional and global hub of innovative research and teaching activity.',
+    majorRole: 'Junior Software Engineer (Intern)',
+    roles: ['Software Development', 'Application Development'],
+    technologies: [
+      'Agile',
+      'Google Maps',
+      'Open street Maps',
+      'Location API',
+      'Git',
+      'Linux',
+    ],
+    languages: ['Java', 'Servlet', 'MySQL', 'Bash_Script'],
+  },
+  {
+    organization: 'Backend development for Freelance',
+    position: 'Working as Backend developer',
+    organizationDescription:
+      'Developing A Restful API for E-commerce system using Node.js with Test driven Development Pattern with clean coding and proper documentation.',
+    majorRole: 'Software Engineer,Programmer',
+    roles: ['Software Development', 'Database Designer', 'Tester'],
+    technologies: ['TDD', 'Agile', 'Swagger', 'JWT', 'Git', 'Linux'],
+    languages: ['Node.js', 'Express', 'MongoDB', 'Jest'],
+  },
+];
--- src/app/constants/skills.ts
+++ src/app/constants/skills.ts
@@ -1,18 +1,42 @@
 export interface SkillDTO {
-    title:string,
-    progressInPercent:string,
-    skillLevel:string
+  title: string;
+  progressInPercent: string;
+  skillLevel: string;
 }
 
-export const SkillInfo:SkillDTO[] = [
-    {title:"Node.js",progressInPercent:"85",skillLevel:"Advanced"},
-    {title:"Typescript",progressInPercent:"90",skillLevel:"Advanced"},
-    {title:"Angular",progressInPercent:"90",skillLevel:"Advanced"},
-    {title:"Flutter,Native Android Development",progressInPercent:"90",skillLevel:"Experienced"},
-    {title:"Java,C++,C",progressInPercent:"78",skillLevel:"Experienced"},
-    {title:"Dart",progressInPercent:"85",skillLevel:"Intermediate"},
-    {title:"Python,Javascript",progressInPercent:"85",skillLevel:"Advanced"},
-    {title:"MySQL,MongoDB,PostgresSQL",progressInPercent:"80",skillLevel:"Advanced"},
-    {title:"Payment Integration(Chapa)",progressInPercent:"70",skillLevel:"Experienced"},
-    {title:"SMS Integration(Twilio)",progressInPercent:"70",skillLevel:"Experienced"},
-]; 
+export const SkillInfo: SkillDTO[] = [
+  {
+    title: 'Node.js, NestJs, Express',
+    progressInPercent: '85',
+    skillLevel: 'Advanced',
+  },
+  { title: 'Typescript', progressInPercent: '90', skillLevel: 'Advanced' },
+  { title: 'Angular, React ', progressInPercent: '90', skillLevel: 'Advanced' },
+  {
+    title: 'Flutter,Native Android Development',
+    progressInPercent: '90',
+    skillLevel: 'Experienced',
+  },
+  { title: 'Java,C++,C', progressInPercent: '78', skillLevel: 'Experienced' },
+  { title: 'Dart', progressInPercent: '85', skillLevel: 'Intermediate' },
+  {
+    title: 'Python,Javascript',
+    progressInPercent: '85',
+    skillLevel: 'Advanced',
+  },
+  {
+    title: 'MySQL,MongoDB,PostgresSQL',
+    progressInPercent: '80',
+    skillLevel: 'Advanced',
+  },
+  {
+    title: 'Payment Integration(Chapa)',
+    progressInPercent: '70',
+    skillLevel: 'Experienced',
+  },
+  {
+    title: 'SMS Integration(Twilio)',
+    progressInPercent: '70',
+    skillLevel: 'Experienced',
+  },
+];
