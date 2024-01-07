945a7da85ff6963c90b2cf84f71eaf226ecedf53
Amanuel Shiferaw <123aman64@gmail.com>
2024-01-07 11:07:59 +0300

    conf: add git ignore file

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 const express = require("express");
 
 const app = express();
-// app.use(express.static("./dist"));
+app.use(express.static("./dist"));
 
 app.get("/*", (req, res) => {
-  res.send("<h2>under maintenance i will be back soon</h2>");
+  res.sendFile("index.html", { root: "dist/" });
 });
-
-// app.get("/root*", (req, res) => {
-//   res.sendFile("index.html", { root: "dist/" });
-// });
 
 app.listen(process.env.PORT || 8080);
