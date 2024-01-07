7548430c6ad43f6b6017bb29081c5f6d2b2531dd
Amanuel Shiferaw <123aman64@gmail.com>
2024-01-07 10:39:56 +0300

    fix: down temporarily

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require("express");
 
 const app = express();
-app.use(express.static("./dist"));
+// app.use(express.static("./dist"));
 
 app.get("/*", (req, res) => {
   res.send("<h2>under maintenance i will be back soon</h2>");
 });
 
-app.get("/root*", (req, res) => {
-  res.sendFile("index.html", { root: "dist/" });
-});
+// app.get("/root*", (req, res) => {
+//   res.sendFile("index.html", { root: "dist/" });
+// });
 
 app.listen(process.env.PORT || 8080);
